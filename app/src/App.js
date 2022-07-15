a40c3ce9d4f93c118833be038b6c7c84f3c186ad
j3nny-zhang <jennyzhanng@gmail.com>
2022-07-15 19:33:03 -0400

    test fetch

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
 function App() {
+
+  App.js.useEffect(function(){
+    fetch('/api/prescription')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, [])
+
   return (
     <div className="App">
       <h1>Hi</h1>
